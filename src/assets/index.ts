aae29ec490a0ca3eeb3ac595b965e372db3a4c0f
deepak kahar <kahardeepak14@gmail.com>
2025-03-16 16:21:06 +0530

    home page completed

---
+++ src/assets/index.ts
@@ -0,0 +1,28 @@
+// icons
+import Call from "./icons/Call.svg";
+import Home from "./icons/Home.svg";
+import Logo from "./astro.svg";
+import ArrowRight from "./icons/ArrowRight.svg";
+import MailLeads from "./icons/MailLeads.svg";
+import MenTalking from "./icons/MenTalking.svg";
+
+// images
+import ImageSeries from "./images/ImageSeries.png";
+import Meta from "./images/meta.jpg";
+import MultipleImage from "./images/MultipleImage.png";
+import SmartServe from "./images/SmartServe.png";
+import CaseStudy from "./images/CaseStudy.png";
+
+export {
+  Call,
+  Home,
+  Logo,
+  Meta,
+  ArrowRight,
+  ImageSeries,
+  MultipleImage,
+  MailLeads,
+  MenTalking,
+  SmartServe,
+  CaseStudy,
+};
---
+++ src/constant/index.ts
@@ -0,0 +1,44 @@
+type ServicesDataType = {
+  id: number;
+  title: string;
+  description: string;
+};
+
+export const ServicesData: ServicesDataType[] = [
+  {
+    id: 1,
+    title: "Startup marketing",
+    description:
+      "Our tailored startup packages offer a full range of digital marketing services, empowering your startup to overcome digital challenges and achieve success.",
+  },
+  {
+    id: 2,
+    title: "Web development",
+    description:
+      "Deliver a captivating visual experience for your customers with a website design that’s seamless to navigate, user-friendly, and perfectly aligned with your brand.",
+  },
+  {
+    id: 3,
+    title: "Crypto marketing",
+    description:
+      "Our Crypto packages feature a range of strategies customized to suit the distinct needs and audience of the ever-evolving crypto landscape.",
+  },
+  {
+    id: 4,
+    title: "Social media marketing",
+    description:
+      "We fuel your growth through in-depth research and tailored strategies, ensuring you reach the right audience & achieve your long-term social media objectives.",
+  },
+  {
+    id: 5,
+    title: "Influencer marketing",
+    description:
+      "Our extensive reach includes both micro and macro influencers, helping to establish strong credibility with your desired audience.",
+  },
+  {
+    id: 6,
+    title: "Digital PR",
+    description:
+      "Our PR strategies are rooted in transparency and a data- driven approach, effectively shaping public opinion in your favor across various touchpoints.",
+  },
+];
